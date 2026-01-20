157e76a030f74b020c2d8012d83a4c5ad103c29b
zbzzz1 <zbz5329@psu.edu>
2026-01-20 12:10:08 -0500

    added motorcyle

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args){
-        Motorcylce ml = new Motorcylce();
+        Motorcycle m1 = new Motorcycle( "RED");
+        System.out.println(m1.getColorA());
+
+        Motorcycle m2 = new Motorcycle( "BLUE");
+        System.out.println(m2.getColorA());
+
     }
 }
--- src/Motorcycle.java
+++ src/Motorcycle.java
@@ -1,3 +1,9 @@
-﻿class Motorcylce {
+public class Motorcycle {
     private String color;
+    public Motorcycle(String color1){
+        this.color = color1;
+    }
+    public String getColorA() {
+        return this.color;
+    }
 }
